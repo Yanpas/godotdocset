a4ac5edb2fd636b2475576d6fc48d27ed8f675c6
Yan Pas <yanp.bugz@gmail.com>
2018-02-23 18:36:56 +0300

    css, toc

--- godotdocset.py
+++ godotdocset.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 '''
-MIT license, author Yan Pas
+MIT license, author Yan Pas <yanp.bugz@gmail.com>
 '''
 
 import jinja2
@@ -17,11 +17,20 @@ from glob import glob
 import sqlite3
 import shutil
 
-def linkify_text(txt):
-	return re.sub(r'\[(.+?)\]', r'<a href="\1.html">\1</a>', txt)
+TEMPLATE = "template.jinja2"
+
+def linkify_text(node: etree.Element) -> str:
+	txt = node.text.strip()
+	txt = txt.replace("\n", "<br />")
+	txt = txt.replace("\t", "")
+	txt = re.sub(r'\[(\w+?)\](.+?)\[/\1\]', r'<span class="tag_\1">\2</span>', txt) # [code]bla[/code] -> <span>...
+	# TODO generate valid links for methods etc.
+	txt = re.sub(r'\[(\w+?) (\w+?)\]', r'\1 \2', txt) # [method foo] -> method foo
+	txt = re.sub(r'\[(\w+?)\]', r'<a href="\1.html">\1</a>', txt) # [Object] -> <a href="..."
+	return txt
 
 def j_print_args(args, **_kwargs):
-	return ", ".join(['<a href="{0}.html">{0}</a> {1}{dflt}'.format(a.type, a.name, dflt=("="+a.default if a.default else "")) for a in args])
+	return ", ".join([f'<a href="{a.type}.html">{a.type}</a> {a.name}{"="+a.default if a.default else ""}' for a in args])
 
 class DocPage:
 	parents = {}
@@ -33,8 +42,8 @@ class DocPage:
 		self.parents = []
 		DocPage.parents[self.title] = self.inherits
 		self.category = root.attrib["category"]
-		self.brief_description = linkify_text(root.find('brief_description').text.strip())
-		self.description = linkify_text(root.find('description').text.strip())
+		self.brief_description = linkify_text(root.find('brief_description'))
+		self.description = linkify_text(root.find('description'))
 		self.populate_methods()
 		self._j_print_args = j_print_args
 		self.populate_signals()
@@ -63,11 +72,11 @@ class DocPage:
 			res.cnt = cnt
 			res.name = method.attrib["name"]
 			res.__dict__["return"] = method.find("return").attrib["type"] if method.find("return") else "void"
-			res.description = method.find("description").text.strip()
+			res.description = linkify_text(method.find("description"))
 			res.arguments = []
 			for arg in method.findall("argument"):
 				res.arguments.append(SimpleNamespace(index=int(arg.attrib["index"]), name=arg.attrib["name"],
-						type=arg.attrib["type"], default=(arg.attrib.get('default'))))
+						type=arg.attrib["type"], default=arg.attrib.get('default')))
 			res.arguments.sort(key=lambda el: el.index)
 			self.methods.append(res)
 			cnt += 1
@@ -77,11 +86,11 @@ class DocPage:
 		for sig in self._root.findall("signals/signal"):
 			res = SimpleNamespace()
 			res.name = sig.attrib['name']
-			res.description = sig.find("description").text.strip()
+			res.description = linkify_text(sig.find("description"))
 			res.arguments = []
 			for arg in sig.findall("argument"):
 				res.arguments.append(SimpleNamespace(index=int(arg.attrib["index"]), name=arg.attrib["name"],
-						type=arg.attrib["type"], default=(arg.attrib.get('default'))))
+						type=arg.attrib["type"], default=None))
 			res.arguments.sort(key=lambda el: el.index)
 			self.signals.append(res)
 		
@@ -90,7 +99,7 @@ class DocPage:
 		for field in self._root.findall("members/member"):
 			res = SimpleNamespace()
 			res.name = field.attrib['name']
-			res.description = field.text.strip()
+			res.description = linkify_text(field)
 			res.type = field.attrib['type']
 			self.fields.append(res)
 
@@ -101,10 +110,10 @@ class DocPage:
 			if ename not in self.consts:
 				self.consts[ename] = []
 			res = SimpleNamespace(name=const.attrib['name'], value=const.attrib['value'])
-			res.description = linkify_text(const.text.strip())
+			res.description = linkify_text(const)
 			self.consts[ename].append(res)
 
-def getPlist(name):
+def get_plist(name: str):
 	return '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -137,7 +146,7 @@ class DocsetMaker:
 		self.db.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
 		self.db.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 		with open(DocsetMaker.outname + '.docset/Contents/Info.plist', 'w') as plist:
-			plist.write(getPlist(DocsetMaker.outname))
+			plist.write(get_plist(DocsetMaker.outname))
 		self.db.execute("BEGIN")
 		return self
 	
@@ -146,23 +155,27 @@ class DocsetMaker:
 		self.db.close()
 	
 	def add_to_docset(self, dp: DocPage):
-		def add_entry(name, type):
-			self.db.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?);', [name, type, dp.title + ".html"])
+		def add_entry(name, entrytype, anchor=""):
+			if anchor:
+				anchor = "#" + anchor
+			self.db.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?);',
+					[name, entrytype, dp.title + ".html" + anchor])
 		add_entry(dp.title, "Class")
 		for e in dp.consts:
+			def add_constants(src):
+				for x in src:
+					add_entry(x.name, "Constant", f'c_{x.name}')
 			if e is None:
-				for x in dp.consts[None]:
-					add_entry(x.name, "Constant")
+				add_constants(dp.consts[None])
 			else:
-				add_entry(e, "Enum")
-				for x in dp.consts[e]:
-					add_entry(x.name, "Constant")
+				add_entry(e, "Enum", "e_" + e)
+				add_constants(dp.consts[e])
 		for e in dp.fields:
-			add_entry(e.name, "Field")
+			add_entry(e.name, "Field", f'f_{e.name}')
 		for e in dp.methods:
-			add_entry(e.name, "Method")
+			add_entry(e.name, "Method", f"m_{e.name}_{e.cnt}")
 		for e in dp.signals:
-			add_entry(e.name, "Subroutine")
+			add_entry(e.name, "Subroutine", "s_"+e.name)
 
 if __name__ == '__main__':
 	ap = argparse.ArgumentParser()
@@ -171,22 +184,18 @@ if __name__ == '__main__':
 	args = ap.parse_args()
 	frompath = args.__dict__['from']
 	
-	tpl = jinja2.Template(open('template.jinja2').read())
 	if not os.path.exists(frompath) or not os.path.isdir(frompath):
 		exit("Directory " + frompath + " doesn't exist or is not a directory")
 	docsetdir = DocsetMaker.outname + ".docset"
 	if os.path.exists(docsetdir):
 		print("Removing docset dir")
 		shutil.rmtree(docsetdir)
-	# doc = etree.parse(frompath)
-	# dp = DocPage(doc.getroot())
-	# render = tpl.render(vars(dp))
-	# dump(render, os.path.basename(frompath))
 	docpages = {}
 	for fname in glob(frompath + "/*.xml"):
 		doc = etree.parse(fname)
 		print("parsing", fname)
 		docpages[fname] = DocPage(doc.getroot())
+	tpl = jinja2.Template(open(TEMPLATE).read())
 	with DocsetMaker() as docset:
 		def dump(content, name):
 			with open(os.path.join(DocsetMaker.docdir, name[:-4] + ".html"), 'w', encoding="utf-8") as f:
@@ -197,3 +206,6 @@ if __name__ == '__main__':
 			docset.add_to_docset(dp)
 			render = tpl.render(vars(dp))
 			dump(render, os.path.basename(fname))
+		shutil.copyfile('style.css', os.path.join(docset.docdir, "style.css"))
+		shutil.copyfile("icon.png", os.path.join(docset.rootdir, "icon.png"))
+		shutil.copyfile("icon.png", os.path.join(docset.rootdir, "icon@2.png"))
